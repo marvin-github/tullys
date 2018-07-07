60e2cedb337152ba6ce50dfc5bb2d1f0b677de2f
marvin konwinski <marvinkonwinski@marvins-MacBook-Pro-2.local>
2018-07-07 09:05:45 -0500

    modify invoice select list, canine and customer

--- app/models/canine.rb
+++ app/models/canine.rb
@@ -3,11 +3,12 @@ class Canine < ApplicationRecord
   belongs_to :weight_category
   belongs_to :registration_company
   belongs_to :sale_status
-  has_many :treatments, :dependent => :destroy
+  has_many :treatments, :dependent => :destroy, validate: false
   accepts_nested_attributes_for :treatments, allow_destroy: true
   #has_many :canine_treatment_types, dependent: :destroy
   #accepts_nested_attributes_for :canine_treatment_types, allow_destroy: true
-
+  validates_presence_of :color
+  validates_presence_of :gender
   validates_presence_of :micro_chip_number
   validates_length_of :micro_chip_number, :minimum => 5, :too_short => "please enter at least 5 characters"
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
   #validates :home_phone, uniqueness: true
+  validates_presence_of :mobile_phone
   validates_uniqueness_of :mobile_phone, :allow_blank => true, :allow_nil => true
   validates_uniqueness_of :home_phone, :allow_blank => true, :allow_nil => true
   validates_format_of :mobile_phone, with: /\A\d{3}-\d{3}-\d{4}\z/, :allow_blank => true, :message => "use 000-000-000 format"
